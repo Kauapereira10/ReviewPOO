a95a381a66add2e302cae429df8b45b1b7afe910
Kaua Pereira <borgeskaua1210@gmail.com>
2026-01-07 17:23:28 -0300

    feat: adicionar segundo livro (book02) e testar métodos
    Adicionado um segundo objeto Book (book02) com dados diferentes de book01.
    Chamados os métodos status(), read() e writedown() para testar o comportamento dos livros.
    Essa atualização ajuda a revisar e praticar conceitos de POO em Java: classes, objetos, estados, atributos e métodos.

--- src/com/kaua/reviewpoo/exercisesTest/BookTest.java
+++ src/com/kaua/reviewpoo/exercisesTest/BookTest.java
@@ -14,11 +14,30 @@ public class BookTest {
 		book01.coverType = "Capa dura";
 		book01.caracteristics = "Ilustrado, Edição Especial, Papel reciclado";
 		book01.launch = "2026-01-10";
-		book01.openBook = true;
+		book01.openBook = false;
 		
 		book01.status();
 		book01.read();
 		book01.writedown();
+		
+		// Objeto book02
+		Book book02 = new Book();
+		book02.nameBook = "Algoritmos e Estruturas de Dados";
+		book02.author = "Ana Souza";
+		book02.publisher = "CodeBooks";
+		book02.pages = 420;
+		book02.price = 99.90;
+		book02.language = "Português";
+		book02.coverType = "Brochura";
+		book02.caracteristics = "Exercícios práticos, Edição 2ª, Conteúdo atualizado";
+		book02.launch = "2025-08-15";
+		book02.openBook = true;
+
+		book02.status();
+		book02.read();
+		book02.writedown();
+
+		
 		
 	}
 }
